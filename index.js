3699f9ddb88cc7094657073a4b56b1e320fd36d2
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-06-30 13:41:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const loop = require('./modules/loop')
+const loop = {
+    Loop: Number.prototype.loop = function(target, callback){
+        let number = this.valueOf()
+        for(let index = 0; index < target; index++){
+            callback(number)
+            number++
+        }
+    },
+    LoopStep: Number.prototype.loopStep = function(target, step, callback){
+        let number = this.valueOf()
+        for(let index = 0; index < target; index++){
+            callback(number)
+            number += step
+        }
+    },
+    DecrementLoop: Number.prototype.decrementLoop = function(target, callback){
+        let number = this.valueOf()
+        for(let index = number; index > target; index--){
+            number--
+            callback(number)
+        }
+    },
+    DecrementStep: Number.prototype.decrementStep = function(target, step, callback){
+        let number = this.valueOf()
+        for(let index = number; index > target; index--){
+            callback(number)
+            number -= step
+        }
+    }
+}
 
-let number = 1
-
-number.loopStep(10, 3, (number)=>{console.log(number)})
+module.exports = loop
