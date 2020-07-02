d263bf6d92e2782000f9c75559c17b4b8437600a
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-07-02 17:40:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const loop = {
     Loop: Number.prototype.loop = function(target, callback){
-        let number = this.valueOf()
+        let number = this.valueOf();
         for(let index = 0; index < target; index++){
-            callback(number)
+            callback(number);
             number++
         }
     },
     LoopStep: Number.prototype.loopStep = function(target, step, callback){
-        let number = this.valueOf()
+        let number = this.valueOf();
         for(let index = 0; index < target; index++){
-            callback(number)
+            callback(number);
             number += step
         }
     },
     DecrementLoop: Number.prototype.decrementLoop = function(target, callback){
-        let number = this.valueOf()
+        let number = this.valueOf();
         for(let index = number; index > target; index--){
             number--
-            callback(number)
+            callback(number);
         }
     },
     DecrementStep: Number.prototype.decrementStep = function(target, step, callback){
-        let number = this.valueOf()
+        let number = this.valueOf();
         for(let index = number; index > target; index--){
-            callback(number)
-            number -= step
+            callback(number);
+            number -= step;
         }
     }
 }
 
-module.exports = loop
+module.exports = loop;
